38364fe690ee95d1fc6dea3c8eee9cf6a3fc0ccc
Priyanshu Saini <priyanshu.se6204@gmail.com>
2025-09-18 15:35:00 +0530

    Added routing stucture for versioned api for problems

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,32 @@
+function PingProblemController(req,res){
+    return res.json({message: 'Problem Controller is up!'});
+};
+
+function addProblem(req,res){
+
+}
+
+function getProblem(req,res){
+
+}
+
+function getProblems(req,res){
+
+}
+
+function updateProblem(req,res){
+
+}
+
+function deleteProblem(req,res){
+
+}
+
+module.exports = {
+    addProblem,
+    getProblem,
+    getProblems,
+    updateProblem,
+    deleteProblem,
+    PingProblemController
+};
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ const bodyparser = require('body-parser');
 
 const app = express();
 const { PORT } = require('./config/server.config.js');
+const apiRouter = require('./routes');
 
 //3 input incoming format for bodyparser
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.text());
 
-
+app.use('/api', apiRouter);
 //const PORT = 3000;
 
 app.get('/ping', async (req, res) => {
